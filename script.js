e0f85f29aaa5f48a836db2e266268967ba721bfc
asaleh123 <140755425+asaleh123@users.noreply.github.com>
2025-01-31 18:13:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    alert("Welcome to my website!");
+    const galleryImages = document.querySelectorAll(".gallery img");
+
+    galleryImages.forEach((img) => {
+        img.addEventListener("click", function () {
+            openLightbox(this.src);
+        });
+    });
+
+    function openLightbox(imageSrc) {
+        let lightbox = document.createElement("div");
+        lightbox.id = "lightbox";
+        lightbox.innerHTML = `
+            <div class="lightbox-content">
+                <span class="close">&times;</span>
+                <img src="${imageSrc}" alt="Expanded Image">
+            </div>
+        `;
+        document.body.appendChild(lightbox);
+
+        document.querySelector(".close").addEventListener("click", function () {
+            document.body.removeChild(lightbox);
+        });
+    }
 });
